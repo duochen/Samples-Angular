9d73955e559cbf994e3606a8bde82b90916c6ea0
duochen <duochen@yahoo.com>
2018-04-20 16:13:58 -0500

    check in code

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,6 +5,12 @@ import { CoursesService } from './courses.service';
     selector: 'courses',   //<courses>
     template: `
         <h2>{{ title }}</h2>
+        <img [src]="imageUrl" />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
@@ -14,7 +20,9 @@ import { CoursesService } from './courses.service';
 })
 export class CoursesComponent {
     title = "List of courses";
+    imageUrl = "http://lorempixel.com/400/200";
     courses; // = ["course1", "course2", "course3"];
+    colSpan = 2;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
