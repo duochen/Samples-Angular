6a2d44bcbe8db2807448c61acdf5c34c9f8f816a
Duo Chen <dchen@slb.com>
2019-09-11 17:23:22 -0500

    check in code

--- IoT-Dashboard/src/app/stock/stock-item/stock-item.component.ts
+++ IoT-Dashboard/src/app/stock/stock-item/stock-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { timingSafeEqual } from 'crypto';
+
+import { Stock } from '../../model/stock';
 
 @Component({
   selector: 'app-stock-item',
@@ -8,31 +9,22 @@ import { timingSafeEqual } from 'crypto';
 })
 export class StockItemComponent implements OnInit {
 
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
-  public positiveChange: boolean;
-  public favorite: boolean;
+  public stock: Stock;
 
   constructor() { }
 
   ngOnInit() {
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85
-    this.previousPrice = 80;
-    this.positiveChange = this.price > this.previousPrice;
-    this.favorite = false;
-  }
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+ }
 
-  toggleFavorite() {
+  toggleFavorite(event) {
     var temp: Boolean;
-    temp = this.favorite;
+    temp = this.stock.favorite;
     console.log('Before we are toggling the favorite state for this stock', temp)
 
-    this.favorite = !temp;
+    this.stock.favorite = !temp;
 
-    console.log('After we are toggling the favorite state for this stock', this.favorite)    
+    console.log('After we are toggling the favorite state for this stock', this.stock.favorite)    
+    console.log('The event is: ', event)    
   }
 }
