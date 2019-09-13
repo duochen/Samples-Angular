ee08760e5ee58ef5027b29c56270a802b135bae5
Duo Chen <dchen@slb.com>
2019-09-13 18:04:22 -0500

    check in code

--- IoT-Dashboard/src/app/app.module.ts
+++ IoT-Dashboard/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { CreateStockComponent } from './stock/create-stock/create-stock.componen
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- IoT-Dashboard/src/app/stock/create-stock/create-stock.component.ts
+++ IoT-Dashboard/src/app/stock/create-stock/create-stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Stock } from '../../model/stock';
 
 @Component({
@@ -7,25 +8,33 @@ import { Stock } from '../../model/stock';
   styleUrls: ['./create-stock.component.scss']
 })
 export class CreateStockComponent {
-  public stock : Stock;
-  public confirmed = false;
-  public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
-  constructor() { 
-    this.stock = new Stock('test', '', 0, 0, 'NASAQ');
-  }
+  // public stock : Stock;
+  // public confirmed = false;
+  // public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
+  // constructor() { 
+  //   this.stock = new Stock('test', '', 0, 0, 'NASAQ');
+  // }
 
-  setStockePrice(price) {
-    this.stock.price = price;
-    this.stock.previousPrice = price;
-  }
+  // setStockePrice(price) {
+  //   this.stock.price = price;
+  //   this.stock.previousPrice = price;
+  // }
 
-  createStock(stockForm) {
-    console.log('Stock form ', stockForm);
-    if (stockForm.valid) {
-      console.log('Createing stock ', this.stock)
-    } else {
-      console.error('Stock form is in an invalid state')
-    }
+  // createStock(stockForm) {
+  //   console.log('Stock form ', stockForm);
+  //   if (stockForm.valid) {
+  //     console.log('Before assigning ', this.stock)
+  //     this.stock = stockForm.value.stock;
+  //     console.log('Createing stock ', this.stock)
+  //   } else {
+  //     console.error('Stock form is in an invalid state')
+  //   }
+  // }
+  public nameControl = new FormControl();
 
+  constructor() {}
+
+  onSubmit() {
+    console.log('Name Control Value', this.nameControl.value);
   }
 }
