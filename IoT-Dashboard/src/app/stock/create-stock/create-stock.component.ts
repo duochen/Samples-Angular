d7f411033abeb498aec034c98cb168ce76917f11
Duo Chen <dchen@slb.com>
2019-09-13 14:22:59 -0500

    check in code

---
+++ IoT-Dashboard/src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from '../../model/stock';
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.scss']
+})
+export class CreateStockComponent {
+  public stock : Stock;
+  constructor() { 
+    this.stock = new Stock('test', '', 0, 0);
+  }
+}
