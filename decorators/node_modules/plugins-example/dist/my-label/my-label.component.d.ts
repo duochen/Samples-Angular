c8e35c2160a6bab0af0db2f61e135cc3bc48b10b
Duo Chen <dchen@slb.com>
2019-09-19 09:47:58 -0500

    check in code

---
+++ decorators/node_modules/plugins-example/dist/my-label/my-label.component.d.ts
@@ -0,0 +1,4 @@
+import { OnInit } from '@angular/core';
+export declare class MyLabelComponent implements OnInit {
+    ngOnInit(): void;
+}
--- plugins/src/app/app.component.ts
+++ plugins/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component, Compiler, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, Compiler, ViewChild, ViewContainerRef, AfterViewInit, NgModule, Injector } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 import { PluginsModule } from './plugins.module';
 import { SettingsComponent } from './settings/settings.component';
+
+import { System } from 'systemjs';
+declare var SystemJS: System;
+
+interface PluginsConfig {
+  system: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild('content', {read: ViewContainerRef, static: false})
   content: ViewContainerRef;
 
@@ -16,12 +25,23 @@ export class AppComponent {
 
   links: {text: string, path: string }[] = [];
 
-  constructor(private compiler: Compiler, private router: Router) {
+  constructor(private compiler: Compiler, private router: Router, private http: HttpClient) {
     this.module = this.compiler.compileModuleAndAllComponentsSync(
       PluginsModule
     );
 
   this.createRoute('Settings', 'settings', SettingsComponent);
+  }
+
+  async ngAfterViewInit() {
+    const url = '/assets/plugins.config.json';
+    const config = <PluginsConfig> await this.http.get(url).toPromise();
+    console.log(config);    
+
+    SystemJS.config(config.system);
+
+    const core = await SystemJS.import('plugins-core');
+    console.log(core);
   }
 
   createView(name: string) {
