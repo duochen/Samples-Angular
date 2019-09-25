0fbb00fc721490b5edeba21624dbc245167690ee
Duo Chen <dchen@slb.com>
2019-09-25 09:59:50 -0500

    check in code

--- reusable-components/ng-framework/modules/header/src/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@ angular/ core'; 
-
-@Component({ 
-	selector: 'ngfw-header', 
-	templateUrl: './ header.component.html', 
-	styleUrls: ['./ header.component.css']  
-}) 
-
-export class HeaderComponent {}
