ab1e5122b9b1eb648cf287dd2f2b98b1fe473582
Duo Chen <dchen@slb.com>
2019-11-06 15:25:41 -0600

    check in code

--- angular-up-and-running/Exercise/ecommerce/src/app/products/product-item/product-item.component.ts
+++ angular-up-and-running/Exercise/ecommerce/src/app/products/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-product-item',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent implements OnInit {
+  public product: Product;
 
   constructor() { }
 
   ngOnInit() {
+    this.product = {
+      name: 'My Test Product',
+      imageUrl: 'http://via.placeholder.com/150x150',
+      price: 50,
+      isOnSale: true,
+      quantityInCart: 0
+    };
+  }
+
+  incrementInCart() {
+    this.product.quantityInCart++;
+  }
+
+  decrementInCart() {
+    this.product.quantityInCart--;
   }
 
 }
