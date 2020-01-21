7043bb8600e06d1b6d2c04f17dc38f7ba54ddb7b
Duo Chen <dchen@slb.com>
2020-01-21 14:58:17 -0600

    check in code

--- ecommerce/src/app/products/product-item/product-item.component.ts
+++ ecommerce/src/app/products/product-item/product-item.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent implements OnInit {
+  public name: string;
+  public price: number;
+  public imageUrl: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Test Stock Company';
+    this.price = 11.2;
+    this.imageUrl = "https://www.gstatic.com/webp/gallery3/1.png"
   }
 
 }
