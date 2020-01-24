99919aaa011e62bb8a8047def2e89dd1bfb45817
Duo Chen <dchen@slb.com>
2020-01-24 16:26:44 -0600

    check in code

--- angular-up-and-running/SourceCode/chapter8/observables/src/app/services/stock.service.ts
+++ angular-up-and-running/SourceCode/chapter8/observables/src/app/services/stock.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse, HttpEvent } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { _throw as ObservableThrow } from 'rxjs/observable/throw';
@@ -14,7 +14,40 @@ export class StockService {
    }
 
   getStocks(): Observable<Stock[]> {
-    return this.http.get<Stock[]>('/api/stock');
+    return this.http.get<Stock[]>('/api/stock', {
+      headers: new HttpHeaders()
+        .set('Authorization', 'MyAuthorizationHeaderValue')
+        .set('X-EXAMPLE-HEADER', 'TestValue'),
+      params: {
+        q: 'test',
+        test: 'value',
+      },
+      observe: 'body'
+    });
+  }
+
+  getStocksAsResponse(): Observable<HttpResponse<Stock[]>> {
+    return this.http.get<Stock[]>('/api/stock', {
+      observe: 'response'
+    });
+  }
+
+  getStocksAsEvents(): Observable<HttpEvent<any>> {
+    return this.http.get('/api/stock', {
+      observe: 'events'
+    });
+  }
+
+  getStocksAsString(): Observable<string> {
+    return this.http.get('/api/stock', {
+      responseType: 'text'
+    });
+  }
+
+  getStocksAsBlob(): Observable<Blob> {
+    return this.http.get('/api/stock', {
+      responseType: 'blob'
+    });
   }
 
   createStock(stock: Stock): Observable<any> {
