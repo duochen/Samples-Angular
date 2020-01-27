96c3159a81a1825d1490ea781573a6191b15d2ce
Duo Chen <dchen@slb.com>
2020-01-27 14:34:49 -0600

    check in code

--- angular-up-and-running/SourceCode/chapter9/simple-http/src/app/services/stock.service.ts
+++ angular-up-and-running/SourceCode/chapter9/simple-http/src/app/services/stock.service.ts
@@ -10,8 +10,8 @@ export class StockService {
 
   constructor(private http: HttpClient) {}
 
-  getStocks() : Observable<Stock[]> {
-    return this.http.get<Stock[]>('/api/stock');
+  getStocks(query: string) : Observable<Stock[]> {
+    return this.http.get<Stock[]>(`/api/stock?q=${query}`);
   }
 
   createStock(stock: Stock): Observable<any> {
--- angular-up-and-running/SourceCode/chapter9/simple-http/src/app/stock/stock-list/stock-list.component.ts
+++ angular-up-and-running/SourceCode/chapter9/simple-http/src/app/stock/stock-list/stock-list.component.ts
@@ -3,6 +3,9 @@ import { StockService } from 'app/services/stock.service';
 import { AuthService } from 'app/services/auth.service';
 import { Stock } from 'app/model/stock';
 import { Observable } from 'rxjs/Observable';
+import { share, debounceTime, switchMap, distinctUntilChanged, startWith } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
+
 
 @Component({
   selector: 'app-stock-list',
@@ -10,8 +13,11 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./stock-list.component.css']
 })
 export class StockListComponent implements OnInit {
-
   public stocks$: Observable<Stock[]>;
+  public searchString: string = '';
+
+  private searchTerms: Subject<string> = new Subject();
+
   constructor(private stockService: StockService,
               private authService: AuthService) { }
 
@@ -19,8 +25,18 @@ export class StockListComponent implements OnInit {
     this.fetchStocks();
   }
 
+  search() {
+    this.searchTerms.next(this.searchString);
+  }
+
   fetchStocks() {
-    this.stocks$ = this.stockService.getStocks();
+    this.stocks$ = this.searchTerms.pipe(
+      startWith(this.searchString),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((query) => this.stockService.getStocks(query)),
+      share()
+      );
     this.stocks$.forEach(s => console.log(s));
   }
 
