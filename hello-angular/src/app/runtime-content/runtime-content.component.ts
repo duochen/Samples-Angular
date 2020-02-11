9cce71b08f54dbf73c3e56ddcc7e5a9cc2b13303
Duo Chen <dchen@slb.com>
2020-02-11 17:22:28 -0600

    check in code

--- hello-angular/src/app/runtime-content/runtime-content.component.ts
+++ hello-angular/src/app/runtime-content/runtime-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, Compiler, ComponentFactory, NgModule, ModuleWithComponentFactories, ComponentRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-runtime-content',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./runtime-content.component.css']
 })
 export class RuntimeContentComponent implements OnInit {
+  @ViewChild('container', {read: ViewContainerRef})
+  container: ViewContainerRef;
 
-  constructor() { }
+  private componentRef: ComponentRef<{}>;
+
+  template = '<div>\nHello, {{name}}\n</div>';
+
+  constructor(private compiler: Compiler) { }
 
   ngOnInit() {
   }
 
+  private createComponentFactorySync(
+    compiler: Compiler,
+    metadata: Component,
+    componentClass: any): ComponentFactory<any> {
+
+      const cmpClass = componentClass || class RuntimeComponent {
+        name = "Denys"
+      };
+
+      const decoratedCmp = Component(metadata)(cmpClass);
+    
+      @NgModule({
+        imports: [CommonModule],
+        declarations: [decoratedCmp]
+      })
+      class RuntimeComponentModule{}
+
+      const module: ModuleWithComponentFactories<any> = 
+        compiler.compileModuleAndAllComponentsSync(RuntimeComponentModule);
+
+      return module.componentFactories.find(
+        f => f.componentType === decoratedCmp
+      );
+    }
+
+    compileTemplate() {
+      let metadata = {
+        selector:`runtme-component-sample`,
+        template: this.template
+      };
+
+      let factory = this.createComponentFactorySync(
+        this.compiler,
+        metadata,
+        null
+      );
+
+      if (this.componentRef) {
+        this.componentRef.destroy();
+        this.componentRef = null;
+      }
+      this.componentRef = this.container.createComponent(factory);
+    }
 }
