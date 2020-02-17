1c07f011af17f3bf4d8e5a56abfc015a0bf1bc74
Duo Chen <duochen@yahoo.com>
2020-02-16 22:13:06 -0600

    check in code

--- ES6/app/app.ts
+++
@@ -1,8 +0,0 @@
-import { TextBoxComponent } from './module1';
-import { Logger} from './logger';
-
-let textBox = new TextBoxComponent('hello world');
-textBox.render();
-
-let logger = new Logger('content');
-logger.info('hello world');
---
+++ ES6/systemjs-example/app/app.js
@@ -0,0 +1,21 @@
+System.register(["./module1", "./logger"], function (exports_1, context_1) {
+    "use strict";
+    var module1_1, logger_1, textBox, logger;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (module1_1_1) {
+                module1_1 = module1_1_1;
+            },
+            function (logger_1_1) {
+                logger_1 = logger_1_1;
+            }
+        ],
+        execute: function () {
+            textBox = new module1_1.TextBoxComponent('hello world');
+            textBox.render();
+            logger = new logger_1.Logger('content');
+            logger.info('hello duo');
+        }
+    };
+});
