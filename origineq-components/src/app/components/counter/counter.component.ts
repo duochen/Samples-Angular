4e51a97b2f996d85544deb631b550c15df079836
Duo Chen <dchen@slb.com>
2020-07-19 22:29:48 -0500

    implement counter compnent

--- origineq-components/src/app/components/counter/counter.component.ts
+++ origineq-components/src/app/components/counter/counter.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent implements OnInit {
-
+  @Input() value = 0;
+  @Output() changed = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  increment() {
+    this.updateValue('inc');
+  }
+
+  decrement() {
+    this.updateValue('dec');
+  }
+
+  private updateValue(action: 'inc' | 'dec') {
+    const delta = (action === 'inc') ? 1 : -1;
+    this.value += delta;
+    this.changed.emit(this.value);
+  }
 }
