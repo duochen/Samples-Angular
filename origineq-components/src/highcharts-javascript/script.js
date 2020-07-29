f21ba3d153c4b1f07e47d72768d5739092904207
Duo Chen <dchen@slb.com>
2020-07-29 18:24:19 -0500

    check in code

--- origineq-components/src/highcharts-javascript/script.js
+++ origineq-components/src/highcharts-javascript/script.js
@@ -1,8 +1,57 @@
+
 Highcharts.chart('chart', {
-    title: { text: 'Sales' },
-    series: [ {
+  // chart: { type: 'pie'},
+  // plotOptions: {
+  //   pie: {
+  //     dataLabels: {
+  //       enabled: true,
+  //       format:
+  //         '{point.percentage:.1f} %'
+  //     }
+  //   }
+  // },
+  // chart: { polar: true},
+
+  title: { text: 'Sales' },
+  subtitle: { text: 'Fiscal Year 2020' },
+  xAxis: [
+    {
+      title: { text: 'Month' },
+      categories: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun' ]
+    }
+  ],
+  yAxis: [
+    {
+      title: { text: 'Sales' },
+      tickInterval: 5
+    }
+  ],
+
+  series: [
+    {
       name: 'Model R',
-      data: [ 5, 8, 11, 19, 7 ]
-    } ]
-  });
-  
+      data: [ [0,5],
+              [1,8],
+              {
+                x: 2,
+                y: 11,
+                name: 'Trade Show',
+                color: '#FF9FFF'
+              },
+              [3,19],
+              [4,7],
+              [5,8]],
+      color: '#2A9FBC'
+    },
+    // {
+    //   name: 'Model R2',
+    //   data: [3, 4, 13, 36, 27, 18],
+    //   color: '#F15B2A'
+    // }
+
+  ],
+
+
+
+  legend: { enabled: true }
+});
