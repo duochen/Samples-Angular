bfa86dfd3098119a44b34ad3a8c80b9ba1dd1975
Duo Chen <dchen@slb.com>
2020-11-12 20:21:24 -0600

    check in code

--- Pluralsight/Angular-GettingStarted/APM-Start/src/app/app.module.ts
+++ Pluralsight/Angular-GettingStarted/APM-Start/src/app/app.module.ts
@@ -1,42 +1,28 @@
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { WelcomeComponent } from './home/welcome.component';
-import { StarComponent } from './shared/star.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { ProdutDetailComponent } from './products/produt-detail.component';
 import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProdutDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot(
       [
-        {path: 'products', component: ProductListComponent},
-        {path: 'products/:id',
-         canActivate: [ProductDetailGuard],
-         component: ProdutDetailComponent
-        },
         {path: 'welcome', component: WelcomeComponent},
         {path: '', redirectTo: 'welcome', pathMatch: 'full'},
         {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
       ]
-    )
+    ),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
