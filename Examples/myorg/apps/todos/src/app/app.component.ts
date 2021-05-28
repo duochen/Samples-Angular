7eb675bc32389936da276e7349ada26e78b48e24
Duo Chen <dchen@slb.com>
2021-05-28 17:30:07 -0500

    check in code

--- Examples/myorg/apps/todos/src/app/app.component.ts
+++ Examples/myorg/apps/todos/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 interface Todo {
@@ -13,9 +14,17 @@ export class AppComponent {
   title = 'todos';
   todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
 
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
   addTodo() {
-    this.todos.push({
-      title: `New todo ${Math.floor(Math.random() * 1000)}`
-    });
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    })
   }
 }
