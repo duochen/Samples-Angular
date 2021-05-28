ecb7bd8733609d7b95164b64f3b521f4fb25cb58
Duo Chen <dchen@slb.com>
2021-05-28 16:33:50 -0500

    check in code

--- Udemy/complete_angular_11/TaskManager/src/app/app-routing.module.ts
+++ Udemy/complete_angular_11/TaskManager/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "about", component: AboutComponent},
+  {path: "", pathMatch: "full", redirectTo: "dashboard"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Udemy/complete_angular_11/TaskManager/src/app/dashboard/dashboard.component.ts
+++ Udemy/complete_angular_11/TaskManager/src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpComingProjects: number;
+  ProjectCost: number;
+  CurrentExpenditure: number;
+  AvailableFunds: number;
 
   ngOnInit(): void {
+    this.Designation = "Team Leader";
+    this.Username = "John Smith";
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
   }
 
 }
